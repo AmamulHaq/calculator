430924282adac8a0338855ade12e1b30cbce9a27
AmamulHaq <mdamamul78692@gmail.com>
2025-05-14 12:25:56 +0545

    int result is fixed

--- calculator.cpp
+++ calculator.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 int main(){
-    float a=4, b=2, result;
+    float a=4, b=2; 
+    int result;
     result=add(a,b);
     result=sub(a,b);
     result=multi(a,b);
